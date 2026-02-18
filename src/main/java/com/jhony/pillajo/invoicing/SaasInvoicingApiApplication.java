3ce5bf64dbcad542c44cec7d1a86ad700c6a0ca9
Jhony Pillajo <jhony_hpc.2000@hotmail.com>
2026-02-17 22:01:45 -0600

    feat(setup): Initialize backend structure and database configuration
    - Initialize spring boot project with gradle
    - Configure postgresql datasource
    - Create base test entity

---
+++ src/main/java/com/jhony/pillajo/invoicing/SaasInvoicingApiApplication.java
@@ -0,0 +1,13 @@
+package com.jhony.pillajo.invoicing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaasInvoicingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaasInvoicingApiApplication.class, args);
+	}
+
+}
